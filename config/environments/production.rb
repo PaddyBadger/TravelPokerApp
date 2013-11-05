695549995c9d9a667bc1fd62eecd72f98344573f
Patricia Estridge <patricia.j.estridge@gmail.com>
2013-11-05 17:00:06 -0500

    removed setup_mail.rb from ignore file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,6 @@ Travelpoker::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   #devise
-  config.action_mailer.default_url_options = { :host => 'travelpoker.co' }
+  config.action_mailer.default_url_options = { :host => 'travelpoker.herokuapp.com' }
   
 end
