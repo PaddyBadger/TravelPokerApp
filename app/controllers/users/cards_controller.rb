5b0cd71fab60aba4f9418975bb5a7fa23ebfad05
Patricia Estridge <patricia.j.estridge@gmail.com>
2014-01-28 15:09:46 -0500

    csv

--- app/controllers/users/cards_controller.rb
+++ app/controllers/users/cards_controller.rb
@@ -1,5 +1,6 @@
 class Users::CardsController < ApplicationController  
   def index
-    @cards = User.find(params[:user_id]).cards.paginate(page: params[:page], per_page: 12)
+    @cards = User.find(params[:user_id]).cards
+    @cards = @cards.paginate(page: params[:page], per_page: 12)
   end
 end
--- app/controllers/users/decks_controller.rb
+++ app/controllers/users/decks_controller.rb
@@ -1,5 +1,11 @@
 class Users::DecksController < ApplicationController
   def index
-    @decks = User.find(params[:user_id]).decks.paginate(page: params[:page], per_page: 12)
+    @decks = User.find(params[:user_id]).decks
+    @decks = @decks.paginate(page: params[:page], per_page: 12)
+  end
+
+  def show
+    @decks = User.find(params[:user_id]).decks
+    @decks = @decks.paginate(page: params[:page], per_page: 12)
   end
 end
